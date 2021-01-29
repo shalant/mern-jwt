dc3e9a3e4b725966aba526a087e8d877bdb80651
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-01-28 22:12:09 -0600

    finished, added private and unprivate routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Todos from './Components/Todos';
+import Admin from './Components/Admin';
+import PrivateRoute from './hocs/PrivateRoute';
+import UnPrivateRoute from './hocs/UnPrivateRoute';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Register from './Components/Register';
 
@@ -11,9 +14,10 @@ function App() {
     <Router>
       <Navbar />
       <Route exact path='/' component={Home} />
-      <Route path='/login' component={Login} />
-      <Route path='/register' component={Register} />
-      <Route path='/todos' component={Todos} />
+      <UnPrivateRoute path='/login' component={Login} />
+      <UnPrivateRoute path='/register' component={Register} />
+      <PrivateRoute path='todos' roles={['user', 'admin']} component={Todos} />
+      <PrivateRoute path='admin' roles={['admin']} component={Admin} />
     </Router>
     );
 }
--- client/src/Services/TodoService.js
+++ client/src/Services/TodoService.js
@@ -2,7 +2,7 @@ export default {
     getTodos: () => {
         return fetch('/user/todos')
             .then(response => {
-                if(response.status != 401) {
+                if(response.status !== 401) {
                     return response.json().then(data => data);
                 } else
                     return {message: {msgBody: "UnAuthorized"}, msgError: true};
@@ -17,7 +17,7 @@ export default {
                 'Content-Type': 'application/json'
             }
         }).then(response => {
-            if(response.status != 401) {
+            if(response.status !== 401) {
                 return response.json().then(data => data);
             } else
                 return {message: {msgBody: "UnAuthorized"}, msgError: true};
